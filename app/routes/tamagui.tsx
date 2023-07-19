ad1874be42540545e070f402c1fbbb83d2cba5e8
Ryan D <git@ryan0x44.com>
2023-07-19 17:56:50 +1000

    Added example of Tamagui components from next starter

---
+++ app/routes/tamagui.tsx
@@ -0,0 +1,45 @@
+import type { V2_MetaFunction } from "@remix-run/cloudflare";
+
+import { YStack, XStack, H1, Paragraph, Separator, Anchor, Button } from 'tamagui';
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Tamagui() {
+  return (
+    <YStack f={1} jc="center" ai="center" p="$4" space>
+      <YStack space="$4" maw={600}>
+        <H1 ta="center">Welcome to Tamagui on Remix!</H1>
+        <Paragraph ta="center">
+          Here's a basic starter to show navigating from one screen to another. This screen uses the
+          same code on Next.js and React Native.
+        </Paragraph>
+
+        <Separator />
+        <Paragraph ta="center">
+          Made by{' '}
+          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
+            @natebirdman
+          </Anchor>
+          ,{' '}
+          <Anchor
+            color="$color12"
+            href="https://github.com/tamagui/tamagui"
+            target="_blank"
+            rel="noreferrer"
+          >
+            give it a ⭐️
+          </Anchor>
+        </Paragraph>
+      </YStack>
+
+      <XStack>
+        <Button href="/">Link to user!</Button>
+      </XStack>
+    </YStack>
+  );
+}
